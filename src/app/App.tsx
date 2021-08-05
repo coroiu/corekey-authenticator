99178f51c67670ad2fd9d266049de4abaa587468
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-05 18:14:34 +0200

    Add routing and prettier.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import './App.css';
 import { ServiceWorkerProvider } from './providers/ServiceWorkerProvider';
 import AccountList from './components/AccountList';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NewAccountPage from './pages/account/NewAccountPage';
 
 function App() {
   return (
     <ServiceWorkerProvider>
-      <div className="App">
-        <AccountList></AccountList>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/account/new">
+            <NewAccountPage></NewAccountPage>
+          </Route>
+          <Route path="/">
+            <AccountList></AccountList>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ServiceWorkerProvider>
   );
 }
---
+++ src/app/pages/account/NewAccountPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function NewAccountPage() {
+  return (
+    <>
+      <Typography variant="h1">Add new account</Typography>
+    </>
+  );
+}
