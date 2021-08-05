ea7c1e229fb834c989d803c2660fdeff214ae6e6
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-05 19:11:33 +0200

    Add fab for adding new account.

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import AccountList from "../components/AccountList";
 import AppBar from "../components/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 export default function HomePage() {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
     <>
       <AppBar title="CoreKey Authenticator" isRoot={true}></AppBar>
       <AccountList></AccountList>
+
+      <Fab
+        color="primary"
+        className={classes.fab}
+        onClick={() => history.push("/account/new")}
+        aria-label="add"
+      >
+        <AddIcon />
+      </Fab>
     </>
   );
 }
