41f406669f80546af4297b543aea504e49a0615e
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-09 23:24:11 +0200

    Extend account input.

--- src/app/components/ManualAccountInput.tsx
+++ src/app/components/ManualAccountInput.tsx
@@ -1,27 +1,73 @@
 import React, { useState } from "react";
 import TextField, { TextFieldProps } from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core";
 import { noop } from "../utils";
+import { AppTheme } from "../Theme";
+
+const useStyles = makeStyles((theme: AppTheme) => ({
+  textInput: {
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 export interface ManualAccountInputProps {
   onChange?: (key: string) => void;
 }
 
+export interface AccountInformation {
+  account: string;
+  issuer: string;
+  key: string;
+}
+
 export default function ManualAccountInput({
   onChange = noop,
 }: ManualAccountInputProps) {
-  const [key, setKey] = useState("");
-  const handleChange: TextFieldProps["onChange"] = (event) => {
-    setKey(event.target.value);
-  };
+  const classes = useStyles();
+  const [accountInformation, setAccountInformation] =
+    useState<AccountInformation>({
+      account: "",
+      issuer: "",
+      key: "",
+    });
+  const handleChange =
+    (property: keyof AccountInformation): TextFieldProps["onChange"] =>
+    (event) => {
+      setAccountInformation((accountInformation) => ({
+        ...accountInformation,
+        [property]: event.target.value,
+      }));
+    };
 
   return (
-    <TextField
-      id="key"
-      label="Key"
-      value={key}
-      onChange={handleChange}
-      variant="outlined"
-      fullWidth
-    />
+    <>
+      <TextField
+        className={classes.textInput}
+        id="issuer"
+        label="Issuer"
+        value={accountInformation.issuer}
+        onChange={handleChange("issuer")}
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        className={classes.textInput}
+        id="account"
+        label="Account"
+        value={accountInformation.account}
+        onChange={handleChange("account")}
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        className={classes.textInput}
+        id="key"
+        label="Key"
+        value={accountInformation.key}
+        onChange={handleChange("key")}
+        variant="outlined"
+        fullWidth
+      />
+    </>
   );
 }
--- src/app/pages/account/NewAccountPage.tsx
+++ src/app/pages/account/NewAccountPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 import AppBar from "../../components/AppBar";
 import ManualAccountInput from "../../components/ManualAccountInput";
 import { AppTheme } from "../../Theme";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme: AppTheme) => ({
   container: {
@@ -22,7 +22,7 @@ export default function NewAccountPage() {
         <Button color="inherit">Save</Button>
       </AppBar>
       <Container className={classes.container}>
-        <ManualAccountInput onChange={setKey}></ManualAccountInput>
+        <ManualAccountInput onChange={setKey} />
       </Container>
     </>
   );
