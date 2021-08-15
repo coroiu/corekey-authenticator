32f2d9b0d5146316d34b51ade9de99da1a46e6af
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-15 12:53:05 +0200

    Add menu icons.

--- src/app/components/AccountCard.tsx
+++ src/app/components/AccountCard.tsx
@@ -1,17 +1,26 @@
 import { makeStyles, Typography } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import { Account } from '../../modules/crypto/core/ports/account.service/account.model';
 import { AppTheme } from '../Theme';
 
 const useStyles = makeStyles((theme: AppTheme) => ({
   root: {
+    display: "flex",
+  },
+  info: {
+    flex: "1 1",
     padding: theme.spacing(2),
   },
   name: {
     fontSize: "1.3rem",
   },
   issuer: {},
+  menu: {
+    flex: "0 1",
+    padding: theme.spacing(2),
+  },
 }));
 
 export interface AccountCardProps {
@@ -23,16 +32,21 @@ export default function AccountCard({ account }: AccountCardProps) {
 
   return (
     <Paper className={classes.root}>
-      <Typography variant="h4" component="h3" className={classes.name}>
-        {account.name}
-      </Typography>
-      <Typography
-        color="textSecondary"
-        variant="subtitle1"
-        className={classes.issuer}
-      >
-        {account.issuer}
-      </Typography>
+      <div className={classes.info}>
+        <Typography variant="h4" component="h3" className={classes.name}>
+          {account.name}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="subtitle1"
+          className={classes.issuer}
+        >
+          {account.issuer}
+        </Typography>
+      </div>
+      <div className={classes.menu}>
+        <MoreHorizIcon />
+      </div>
     </Paper>
   );
 }
