051d7ca321236e5dc6e74446bbe0b59490d208ab
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-20 14:21:12 +0200

    Implement ability to switch camera.

--- src/app/slides/NewAccountCodeScanSlide.tsx
+++ src/app/slides/NewAccountCodeScanSlide.tsx
@@ -1,4 +1,8 @@
 import { makeStyles } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import SwitchCameraOutlinedIcon from '@material-ui/icons/SwitchCameraOutlined';
 import QrScanner from 'qr-scanner';
 import { useEffect, useRef, useState } from 'react';
 
@@ -21,6 +25,16 @@ const useStyles = makeStyles((theme: AppTheme) => ({
     height: "100%",
     background: theme.palette.common.black,
   },
+  camera: {
+    position: "absolute",
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+    backgroud: "rgba(0, 0, 0, 0.25)",
+
+    "& .MuiSvgIcon-root": {
+      color: theme.palette.common.white,
+    },
+  },
 }));
 
 function NewAccountCodeScanSlide({ close }: SlideProps) {
@@ -29,16 +43,37 @@ function NewAccountCodeScanSlide({ close }: SlideProps) {
   const { showSnackbar } = useSnackbar();
   const videoElRef = useRef<HTMLVideoElement | null>(null);
   const qrScannerRef = useRef<QrScanner | null>();
+  const [cameras, setCameras] = useState<QrScanner.Camera[]>([]);
+  const [cameraMenuAnchorEl, setCameraMenuAnchorEl] =
+    useState<HTMLElement | null>(null);
+
+  const handleCameraClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setCameraMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleCameraClose = () => {
+    setCameraMenuAnchorEl(null);
+  };
+
+  const handleCameraChange = (camera: QrScanner.Camera) => {
+    handleCameraClose();
+    qrScannerRef.current?.setCamera(camera.id);
+  };
 
   useEffect(() => {
     (async () => {
       if (videoElRef.current === null) return;
 
-      qrScannerRef.current = new QrScanner(videoElRef.current, (result) =>
-        console.log("decoded qr code:", result)
+      qrScannerRef.current = new QrScanner(
+        videoElRef.current,
+        (result) => console.log("decoded qr code:", result),
+        undefined,
+        undefined,
+        "environment"
       );
 
       try {
+        setCameras(await QrScanner.listCameras(true));
         await qrScannerRef.current.start();
       } catch (ex) {
         showSnackbar({ message: "Camera not available, using manual input." });
@@ -47,12 +82,36 @@ function NewAccountCodeScanSlide({ close }: SlideProps) {
     })();
 
     return () => qrScannerRef.current?.stop();
-  }, [videoElRef.current]);
+  }, [videoElRef.current, setCameras]);
 
   return (
     <div className={classes.root}>
       NewAccountCodeScanSlide
       <video ref={videoElRef} className={classes.video}></video>
+      {cameras.length <= 1 ? null : (
+        <>
+          <IconButton
+            aria-label="switch camera"
+            className={classes.camera}
+            onClick={handleCameraClick}
+          >
+            <SwitchCameraOutlinedIcon fontSize="large" />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={cameraMenuAnchorEl}
+            keepMounted
+            open={Boolean(cameraMenuAnchorEl)}
+            onClose={handleCameraClose}
+          >
+            {cameras.map((c) => (
+              <MenuItem onClick={() => handleCameraChange(c)}>
+                {c.label}
+              </MenuItem>
+            ))}
+          </Menu>
+        </>
+      )}
     </div>
   );
 }
