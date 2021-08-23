08266ca695ced4c88edf31cc66b3d00b99adfa92
Andreas Coroiu <andreas.coroiu@gmail.com>
2021-08-23 20:52:34 +0200

    Clean up unused classes.

--- src/common/messages/events/account-code-generated.event.ts
+++
@@ -1,9 +0,0 @@
-import { Event } from '../event';
-
-export interface AccountCodeGeneratedEvent extends Event {
-  eventType: 'AccountCodeGenerated';
-}
-
-export function isAccountCodeGeneratedEvent(event: Event): event is AccountCodeGeneratedEvent {
-  return event.eventType === 'AccountCodeGeneratedEvent';
-}
