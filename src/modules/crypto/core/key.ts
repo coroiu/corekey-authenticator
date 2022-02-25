ce8d0553f81bea033079ec19b8d85b4269d42c0a
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-02-25 20:37:34 +0100

    Add saled key variants

--- src/modules/crypto/core/key.ts
+++ src/modules/crypto/core/key.ts
@@ -40,3 +40,47 @@ export class PlainHKey extends PlainKey {
 }
 
 export class PlainTKey extends PlainKey {}
+
+export abstract class SealedKey extends Key {
+  constructor(public readonly cryptoKey: CryptoKey, length: number) {
+    super(length);
+  }
+
+  get method() {
+    const algorithm = this.cryptoKey.algorithm as HmacKeyAlgorithm;
+    return mapHashNameToMethod(algorithm.hash.name);
+  }
+}
+
+export class SealedHKey extends SealedKey {
+  constructor(
+    cryptoKey: CryptoKey,
+    length: number,
+    private _counter: number = 0
+  ) {
+    super(cryptoKey, length);
+  }
+
+  get counter(): number {
+    return this._counter;
+  }
+
+  next(): number {
+    return ++this._counter;
+  }
+}
+
+export class SealedTKey extends SealedKey {}
+
+function mapHashNameToMethod(hashName: string): Method {
+  switch (hashName) {
+    case "SHA-1":
+      return "sha1";
+    case "SHA-256":
+      return "sha256";
+    case "SHA-512":
+      return "sha512";
+    default:
+      throw new Error(`Unsupport crypto key hash method ${hashName}`);
+  }
+}
